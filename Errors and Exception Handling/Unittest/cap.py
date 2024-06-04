6bd528a7be60fae8462bdc922dbe8b557bcc8238
HarsheetT <tharsheet2912@gmail.com>
2024-06-04 19:59:19 +0530

    Error and Exception Handling Lecture

---
+++ Errors and Exception Handling/Unittest/cap.py
@@ -0,0 +1,7 @@
+def cap_text(text):
+    '''
+    Input a string
+    Output the Capitalized String
+    '''
+    # return text.capitalize() --> Only capitalize the first letter --> Throws error
+    return text.title()
